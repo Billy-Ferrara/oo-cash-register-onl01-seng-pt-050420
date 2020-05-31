c8ecaffafcd86fa9a88c8b58d71d0f077a4551f9
Billy Ferrara <willfira717@gmail.com>
2020-05-31 20:49:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,71 +1,34 @@
-# class CashRegister
-#
-#   attr_accessor  :items, :discount, :total
-#
-#   def initialize(discount=0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-#
-#   def add_item(title, price, quantity = 1)
-#     @total += price * quantity
-#     quantity.times do
-#       @items << title
-#     end
-#
-#     @last_total = @total
-#   end
-#
-#   def apply_discount
-#     if @discount != 0
-#     @total -= (@total * @discount.to_f / 100).to_i
-#       "After the discount, the total comes to $#{@total}."
-#     else
-#       "There is no discount to apply."
-#     end
-#   end
-#
-#   def void_last_transaction
-#     @items.delete_at(-1)
-#     self.total = self.total - @last_total
-#   end
-#
-# end
-
 class CashRegister
-  attr_accessor :discount, :total
+
+  attr_accessor  :items, :discount, :total
 
   def initialize(discount=0)
-    @discount = discount
     @total = 0
+    @discount = discount
     @items = []
-    @last_item = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do
+      @items << title
+    end
+
+    @last_total = @total
   end
 
   def apply_discount
-    if discount != 0
-      @total -= (@total * (@discount / 100.to_f))
-      "After the discount, the total comes to $#{@total.to_i}."
+    if @discount != 0
+    @total -= (@total * @discount.to_f / 100).to_i
+      "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
     end
   end
 
-  def add_item(title, price, quantity=1)
-    @total += price * quantity
-    quantity.times do
-      @items << title
-    end
-    @last_item = price * quantity
-  end
-
-  def items
-    @items
-  end
-
   def void_last_transaction
-     @items.delete_at(-1)
-    self.total = self.total - @last_item
+    @items.delete_at(-1)
+    self.total = self.total - @last_total
   end
+
 end
