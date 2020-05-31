789b960bf031591262b9f636dbaa3377db5b862e
Billy Ferrara <willfira717@gmail.com>
2020-05-31 19:49:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-     
+
     attr_accessor :total, :discount
 
   def initialize(discount=20)
@@ -8,7 +8,6 @@ class CashRegister
     @discount = discount
   end
 
-  def discount
-  end
+  
 
 end
