a90935059737b071af160fb539c07e53a370049a
Billy Ferrara <willfira717@gmail.com>
2020-05-31 20:25:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,11 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
+    quantity.times do 
+      @items << title
+    end 
     @total += price * quantity
+    
   end
 
   def apply_discount
@@ -21,6 +25,7 @@ class CashRegister
     end
   end
 
+  
 
 
 end
